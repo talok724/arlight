1d235d7e3dc3d9aec47ae71cb44b7878e98e0894
Alok Tripathi <127455536+talok724@users.noreply.github.com>
2025-08-29 16:42:17 +0530

    Update custom.js
    added some animation to the images

--- js/custom.js
+++ js/custom.js
@@ -1,73 +1,93 @@
-(function() {
-	'use strict';
+document.addEventListener('DOMContentLoaded', function () {
+    'use strict';
 
-	var tinyslider = function() {
-		var el = document.querySelectorAll('.testimonial-slider');
+    // Tiny Slider initialization for testimonial slider
+    var testimonialSlider = document.querySelector('.testimonial-slider');
+    if (testimonialSlider) {
+        tns({
+            container: testimonialSlider,
+            items: 1,
+            axis: "horizontal",
+            controlsContainer: "#testimonial-nav",
+            swipeAngle: false,
+            speed: 100,
+            nav: true,
+            controls: true,
+            autoplay: true,
+            autoplayHoverPause: true,
+            autoplayTimeout: 2500,
+            autoplayButtonOutput: false
+        });
+    }
 
-		if (el.length > 0) {
-			var slider = tns({
-				container: '.testimonial-slider',
-				items: 1,
-				axis: "horizontal",
-				controlsContainer: "#testimonial-nav",
-				swipeAngle: false,
-				speed: 700,
-				nav: true,
-				controls: true,
-				autoplay: true,
-				autoplayHoverPause: true,
-				autoplayTimeout: 3500,
-				autoplayButtonOutput: false
-			});
-		}
-	};
-	tinyslider();
+    // Tiny Slider initialization for image grid slider
+    var imageGridSlider = document.querySelector('.slider-imgs-grid');
+    if (imageGridSlider) {
+        tns({
+            container: imageGridSlider,
+            items: 1,
+            slideBy: 'page',
+            autoplay: true,
+            autoplayButtonOutput: false,
+            mouseDrag: true,
+            controls: false,
+            nav: true,
+            speed: 800,
+            autoplayTimeout: 10000,
+            responsive: {
+                768: {
+                    items: 1
+                }
+            }
+        });
+    }
 
-	
+    // Initialize all product sliders
+    var productSliders = document.querySelectorAll('.product-slider');
+    if (productSliders.length > 0) {
+        productSliders.forEach(function(slider) {
+            tns({
+                container: slider,
+                items: 1,
+                autoplay: true,
+                autoplayButtonOutput: false,
+                mouseDrag: true,
+                controls: false,
+                nav: true,
+                speed: 1500,
+                autoplayTimeout: 3500,
+                navPosition: "bottom",
+            });
+        });
+    }
 
+    // Quantity counter functionality
+    var sitePlusMinus = function() {
+        var quantity = document.getElementsByClassName('quantity-container');
 
-	var sitePlusMinus = function() {
+        function createBindings(quantityContainer) {
+            var quantityAmount = quantityContainer.getElementsByClassName('quantity-amount')[0];
+            var increase = quantityContainer.getElementsByClassName('increase')[0];
+            var decrease = quantityContainer.getElementsByClassName('decrease')[0];
+            increase.addEventListener('click', function (e) {
+                var value = parseInt(quantityAmount.value, 10);
+                value = isNaN(value) ? 0 : value;
+                value++;
+                quantityAmount.value = value;
+            });
+            decrease.addEventListener('click', function (e) {
+                var value = parseInt(quantityAmount.value, 10);
+                value = isNaN(value) ? 0 : value;
+                if (value > 0) value--;
+                quantityAmount.value = value;
+            });
+        }
 
-		var value,
-    		quantity = document.getElementsByClassName('quantity-container');
-
-		function createBindings(quantityContainer) {
-	      var quantityAmount = quantityContainer.getElementsByClassName('quantity-amount')[0];
-	      var increase = quantityContainer.getElementsByClassName('increase')[0];
-	      var decrease = quantityContainer.getElementsByClassName('decrease')[0];
-	      increase.addEventListener('click', function (e) { increaseValue(e, quantityAmount); });
-	      decrease.addEventListener('click', function (e) { decreaseValue(e, quantityAmount); });
-	    }
-
-	    function init() {
-	        for (var i = 0; i < quantity.length; i++ ) {
-						createBindings(quantity[i]);
-	        }
-	    };
-
-	    function increaseValue(event, quantityAmount) {
-	        value = parseInt(quantityAmount.value, 10);
-
-	        console.log(quantityAmount, quantityAmount.value);
-
-	        value = isNaN(value) ? 0 : value;
-	        value++;
-	        quantityAmount.value = value;
-	    }
-
-	    function decreaseValue(event, quantityAmount) {
-	        value = parseInt(quantityAmount.value, 10);
-
-	        value = isNaN(value) ? 0 : value;
-	        if (value > 0) value--;
-
-	        quantityAmount.value = value;
-	    }
-	    
-	    init();
-		
-	};
-	sitePlusMinus();
-
-
-})()
+        if (quantity.length > 0) {
+            for (var i = 0; i < quantity.length; i++ ) {
+                createBindings(quantity[i]);
+            }
+        }
+    };
+    sitePlusMinus();
+});
